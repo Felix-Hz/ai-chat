b18207781c97fb724cd5b17d60cfc36894989ecd
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-02-29 10:17:41 +0100

    add: prop interface

--- src/app/components/home/AssistantStatus/AssistantStatus.tsx
+++ src/app/components/home/AssistantStatus/AssistantStatus.tsx
@@ -1,8 +1,12 @@
-export const AssistantStatus = ({ status }) => {
-    return (
-      <div className="px-4 py-3 border-b border-gray-300 flex justify-center items-center">
-        <p className="inline text-2xl">👩🏻‍💻</p>
-        <p className="inline pl-2 opacity-75 text-gray-800">{status}</p>
-      </div>
-    );
-  };
+type AssistantStatusProps = {
+  status: string;
+};
+
+export const AssistantStatus: React.FC<AssistantStatusProps> = ({ status }) => {
+  return (
+    <div className="px-4 py-3 border-b border-gray-300 flex justify-center items-center">
+      <p className="inline text-2xl">👩🏻‍💻</p>
+      <p className="inline pl-2 opacity-75 text-gray-800">{status}</p>
+    </div>
+  );
+};
