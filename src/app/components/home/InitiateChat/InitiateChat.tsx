410db268aa73e4f63c3c10dc048b73a030d4ff66
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-03-06 08:32:43 +0100

    fix: cmmt api call

--- src/app/components/home/InitiateChat/InitiateChat.tsx
+++ src/app/components/home/InitiateChat/InitiateChat.tsx
@@ -7,10 +7,10 @@ import { WindowChat } from "../WindowChat/WindowChat";
 import InputMessage from "../InputMessage/InputMessage";
 import { AssistantStatus } from "../AssistantStatus/AssistantStatus";
 
-const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+//   dangerouslyAllowBrowser: true,
+// });
 
 export const InitiateChat = () => {
   const [userInput, setUserInput] = useState("");
